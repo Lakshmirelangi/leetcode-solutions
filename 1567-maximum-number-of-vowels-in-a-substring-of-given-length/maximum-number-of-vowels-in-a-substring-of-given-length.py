293a189466f22fb2db3871a0c6de401a7b6b6a32
Relangi Nagavenkatalakshmi <relanginagavenkatalakshmi@gmail.com>
2026-08-29 05:28:51 +0530

    Time: 55 ms (49.52%) | Memory: 19.8 MB (65.73%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,14 +1,9 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        answer = 0
-        for i in range(0, len(s)-k+1):
-            temp = 0
-            sub_str = s[i:i+k]
-            temp += sub_str.count('a')
-            temp += sub_str.count('e')
-            temp += sub_str.count('i')
-            temp += sub_str.count('o')
-            temp += sub_str.count('u')
-
-            answer = max(temp, answer)
-        return answer
+        vowels = frozenset("aeiou")
+        cnt = ans = sum(s[i] in vowels for i in range(k))
+        if ans != k:
+            for i in range(k, len(s)):
+                cnt += (s[i] in vowels) - (s[i - k] in vowels)
+                ans = max(cnt, ans)
+        return ans
