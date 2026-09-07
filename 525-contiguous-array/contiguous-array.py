7eafe0d62cfdba6fda7227379e0739ab658c5290
Relangi Nagavenkatalakshmi <relanginagavenkatalakshmi@gmail.com>
2026-09-07 09:56:15 +0530

    Time: 75 ms (75.03%) | Memory: 25.3 MB (31.76%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMaxLength(self, nums):
+        prefix_sum = 0
+        first_seen = {0: -1}
+        max_length = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                prefix_sum -= 1
+            else:
+                prefix_sum += 1
+
+            if prefix_sum in first_seen:
+                length = i - first_seen[prefix_sum]
+                max_length = max(max_length, length)
+            else:
+                first_seen[prefix_sum] = i
+
+        return max_length
